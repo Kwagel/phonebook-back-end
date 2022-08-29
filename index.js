07a58677a98def6142e6f5f4f80de2ed198f6622
Kwagel <92309850+Kwagel@users.noreply.github.com>
2022-08-29 10:23:02 +0100

    changed to use process.env.port so heroku works again

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 morgan('dev')
-const PORT = process.env.PORT | 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
